63d1b0f915afd32a4a6e84acbe67666c34900482
codessi <johansuh@gmail.com>
2022-07-22 11:17:53 -0700

    comments made, not full filled

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  // change title on html
+  // Github Finder
+  // react-router-dom  react-icon
+  // folder componenets
+  // subfolder layout
+  // file Navbar.jsx
+  // import Navbar
+  // import BrowserRouter as router, route, routes
+  // wrap Router  insert Navbar
+  //  .flex flex-co justify-between h-screen wrap navbarwith div
+  // insert main content after Navbar
+  // go to Navbar
+  // import FaGithub
+  // improt Link
+  // im port PropTypes
+  // pass title  destuctured prop
+  //  defaultProps set  object with  title to "Github"
+  // proTypes string
+  // edit Jsx nav tag with class 'navbar mb-12 shadow-lg bg-neutral text-neutrol-content' wrap Navbar
+  // .container mx-auto wrap Navbar
+  // .flex-none px-2 mx2 wrap <FaGithub> with .inline pr-2 text-3xl
+  // insert after FA.. Link tag  wrap prop title
+  // add .text-lg font-bold align-middle
+  // outside of parent of link
+  // inert .flex-1 px-2 mx2 wrap
+  // .flex justify-end wrap
+  // Link tag to='/' .btn btn-ghost <- actlike link with hover effect
+  // btn-sm rounded-btn
+  // copy make about 
   return (
     <div className= "bg-purple-500">
       <h1 className="text-xl">hello world</h1>
