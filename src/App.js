c7eb8eb1678197408659de0f2490f76e8fe0769e
codessi <johansuh@gmail.com>
2022-07-22 14:09:48 -0700

    routes working

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Footer from "./components/layout/Footer";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
+import Home
+  from "./pages/Home";
 
 function App() {
  
@@ -9,7 +14,15 @@ function App() {
     <Router>
       <div className="flex flex-col justify-between h-screen">
         <Navbar />
-        <main>content</main>
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/" element={<NotFound />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer /> 
       </div>
     </Router>
   );
