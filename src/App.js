7b3427dde948aefd27a2737a9cbe325ec4e25c33
codessi <johansuh@gmail.com>
2022-07-23 16:10:42 -0700

    comment fullfilled

--- src/App.js
+++ src/App.js
@@ -6,39 +6,47 @@ import Footer from "./components/layout/Footer";
 import About from "./pages/About";
 import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
-
+import { GithubProvider } from "./context/github/GithubContext";
 
 function App() {
-
-  // ceate UserItem.jsx  [x]
-  // impojrt result  [x]
-  // reaplce login with UsrItem  [x]
-  // key user.id user=user as prop  [x]
-  // goto UserItem  [x]
-  // pass prop and destucture user by login and avatar_url  [x]
-  // for div class card shadow-md compact side bg-base-100  [x]
-  // inside another div class .flex-row item-cemter space-x-4 card-body  [x]
-  // inside div  [x]
-  // inside div class avatar  [x]
-  // isside div class rounded-full shadow w-14 h-14  [x]
-  // img src=avatar_url from prop alt=Profile  [x]
-  // 
+  // in src create folder context  [x]
+  // crate folder inside call github  []
+  // create a file  GithubContext.js  []
+  // import creatContext  []
+  // const GITHUB_URL = process.env.....  []
+  // const GITHUB_TOKEN = .....  []
+  // export GithubProvider  []
+  // bringin states from the result  []
+  // bring fetchUsers from  []
+  // return <GithubContext.Provider> wrap {childeren} add value   []attribute object users, loading  []
+  // export default GithubContext  []
+  // go to app.js    []
+  // import GithubProvider }  []
+  // wrap whole jsx githubprovider  []
+  //  checkout on react dev tool  []
+  // goto context, add value fetchUsers  []
+  // goto result delete all the unnessary  []
+  // useContext  bringin pull the value  []
+  // check browser  []
+  // react dev tool   []
 
   return (
-    <Router>
-      <div className="flex flex-col justify-between h-screen  bg-gray-700 text-primary-content footer-center">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/" element={<NotFound />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+    <GithubProvider>
+      <Router>
+        <div className="flex flex-col justify-between h-screen  bg-gray-700 text-primary-content footer-center">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/" element={<NotFound />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
+        </div>
+      </Router>
+    </GithubProvider>
   );
 }
 
