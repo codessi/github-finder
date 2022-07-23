bfe6062e4b8e462d2d57442d38bc5c76c4d133f7
codessi <johansuh@gmail.com>
2022-07-23 10:12:02 -0700

    api study, comment made

--- src/App.js
+++ src/App.js
@@ -9,10 +9,26 @@ import Home
   from "./pages/Home";
 
 function App() {
- 
+    // open up  github docs resource rest api  [x]
+ // goto post man  [x]
+
+  // / users on postman  [x]
+  // users/myanme  [x]
+  // search/users?q=johan  [x]
+  // header-limit  10 per min   [x]
+  // to change create token
+  // go to  setting on my account github []
+  // dev setting -> personal access token  []
+  // click user generalte token  []
+  // postman => authorization => bearer token  []
+  // react -> .env create -> REACT_APP_GITHUB_TOKE  []
+  // REACT_APP_GITHUB_URL= "https://api.github.com"  []
+  // goto home.js  []
+  // {process.env.REACT_APP.....}  []
+
   return (
     <Router>
-      <div className="flex flex-col justify-between h-screen">
+      <div className="flex flex-col justify-between h-screen  bg-gray-700 text-primary-content footer-center">
         <Navbar />
         <main className="container mx-auto px-3 pb-12">
           <Routes>
