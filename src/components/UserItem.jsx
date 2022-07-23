4d5f3b172f913c4b69d0b4e2025a6cb35d811097
codessi <johansuh@gmail.com>
2022-07-23 16:11:18 -0700

    comment fullfilled context

---
+++ src/components/UserItem.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import {Link} from "react-router-dom"
+const UserItem = ({ users: { login, avatar_url } }) => {
+  return (
+    <div className="card shadow-md compact side bg-base-100">
+      <div className="flex-row item-center text-black space-x-4 card-body">
+        <div>
+          <div className="avatar">
+            <div className="rounded-full shadow w-14 h-14">
+              <img src={avatar_url} alt="Profile" />
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <h2 className="card-title">{login}</h2>
+          <Link className="text-black text-opacity-40" to = {`/users/${login}`}> Visit Profile 
+          </Link>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserItem;
