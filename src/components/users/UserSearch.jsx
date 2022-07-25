cff8aa3dabca12ed0e0d2560c8a3c20e0c309472
codessi <johansuh@gmail.com>
2022-07-25 09:47:42 -0700

    user search compnent comment made fullfilled

--- src/components/users/UserSearch.jsx
+++ src/components/users/UserSearch.jsx
@@ -1,14 +1,34 @@
 import React from 'react'
+import { useState, useContext } from 'react'
+import GithubContext from '../../context/github/GithubContext'
 
 
 const UserSearch = () => {
+
+  const {users} = useContext(GithubContext)
+  const [text, setText] = useState("")
+
+  const handleChange = (e) => {
+  setText(e.target.value)
+  }
+  
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (text === "") {
+      alert('Please enter something')
+    } else {
+      // 
+    }
+    setText("")
+  }
+
   return (
     <div className="grid grid-cols-1 xl:grid-cols-2 lg:grid-cols-2 md:grid-cols-2 mb-8 gap-8 wrap">
       <div>
-        <form action="">
+        <form onSubmit={ handleSubmit } action="">
           <div className="form-control">
             <div className="relative">
-              <input type="text" className="w-full pr-40 bg-gray-200 input input-lg text-black" placeholder='Search' />
+              <input type="text" onChange={handleChange}  value={text} className="w-full pr-40 bg-gray-200 input input-lg text-black" placeholder='Search' />
               <button className="absolute top-0 right-0 rounded-l-none w-36 btn btn-lg" type='submit'>
                 Go
               </button>
@@ -16,9 +36,12 @@ const UserSearch = () => {
           </div>
         </form>
       </div>
-      <div className=''  >
+      {users.length > 0 && (
+        <div >
         <button className="btn btn-ghost btn-lg">Clear</button>
       </div>
+      )}
+      
     </div>
   )
 }
