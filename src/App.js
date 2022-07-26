040f24e83f1dd840c6bba68566262ad25adee834
codessi <johansuh@gmail.com>
2022-07-26 05:06:00 -0700

    .

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import NotFound from "./pages/NotFound";
 import Home from "./pages/Home";
 import { GithubProvider } from "./context/github/GithubContext";
 import { AlertProvider } from "./context/alert/AlertContext";
+import Alert from "./components/layout/Alert";
 
 function App() {
   // *** user profile and alert
@@ -17,19 +18,19 @@ function App() {
   // pull setAlert from AlertConterxt  [x]
   // reaplce alert on handle submit with setAlert( with msg and error "please.." "error")  [x]
   // checkout dev tool  [x]
-  // click go empty and checkon  alert Provider  []
-  // create Alert.jsz in layout  []
-  // gt app.js  []
-  // imporrt Alert  []
-  // insert in above switch  []
-  // gt Alert.js  []
-  // rface import usecontext  []
-  // import AlertContext  []
-  // bring in alert from AlertContext using hook  []
-  // jsx  []
+  // click go empty and check on  alert Provider  [x]
+  // create Alert.jsz in layout  [x]
+  // gt app.js  [x]
+  // imporrt Alert  [x]
+  // insert in above Routes [x]
+  // gt Alert.js  [x]
+  // rface import usecontext  [x]
+  // import AlertContext  [x]
+  // bring in alert from AlertContext using hook  []x
+  // jsx  [x]
   // if alert is not null && (p.flex.items-start.md-4.space-x-2)  []
 // {alert.type === 'error' && ( inser SVG)}    []
-  // 
+  // p.flex-1.text-base.font-semibold.leading-7.text-white
 
   return (
     <GithubProvider>
@@ -38,6 +39,7 @@ function App() {
           <div className="flex flex-col justify-between h-screen  bg-gray-700 text-primary-content ">
             <Navbar />
             <main className="container mx-auto px-3 pb-12">
+              <Alert />
               <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
