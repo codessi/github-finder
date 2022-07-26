ab1b0cf8f2ecd1d9dd4e67d2a84cf35da43272da
codessi <johansuh@gmail.com>
2022-07-26 11:13:08 -0700

    comment make not fullfilled

--- src/App.js
+++ src/App.js
@@ -12,40 +12,31 @@ import Alert from "./components/layout/Alert";
 import User from "./pages/User";
 
 function App() {
-// gt userItem.js [x]
-  // change link users to user   [x]
-  // click on visit profile on browser   [x]
-  //gt appjs   [x]
-  // insert  route componet user   [x]
-  // path user/:login   [x]
-  // check on browser   [x]
-  // gt Githubcontext   [x]
-  // add value user: state.user   [x]
-  // copy search user   [x]
-  // change to single user   [x]
-  // pass login   [x]
-  // delete params   [x]
-  // change enpoint /users/${login}   [x]
-  // add after response,  if response.status =-- 404 then window.location = '/notfound'   [x]
-  // else {}  isert rest in   [x]
-  // reaplce items to data   [x]
-  // dispatch change users to user and item to data   [x]
-  // gt github reducer   [x]
-  // add case "GET_USER"   [x]
-  // return {...state,user: action.payload, loading: false}   [x]
-  // gt context   []
-  // add value of getUser   [x]
-  // gt user.jsx   [x]
-  // effect context ghcontext  import   [x]
-  // getuser, user from context   [x]
-  // useEffect take in getUser() pass params.login   [x]
-  // ** router provides match  
-  // import useParam  from react-router-dom   [x]
-  // create params = useParams()   [x]
-  // check devtool gitub provider   [x]
-  // check state   [x]
-  // insert user.login on jsx    [x]
 
+// gt user.jsx
+  //import icon from fa  facodepen store userfriends users
+    //  Link spinner
+  // bring loding from context
+  // add loading under effect
+  // return spinner
+  // check spinner flash on browser
+  // above loading  destructure user object { name, type.  14 items }
+  // jsx  fragment <>
+  // .w-full.mx-auto.lg:w-10/12 warp
+  // insert .mb-4 wrap
+  // insert Link to='/' class='btn btn-ghost'
+  // content Back To Search
+  // outsidd of .mb-4 div -sibling
+  // .grid.grid-cols-1.xl:grid-col-3.lg:grid-cols-3.md:grid-cols-3.mb-8.md:gap-8 wrap
+  // insert .custom-card-image.mb-6.md:mb-0 wrap
+  // insert .rounded-lg.shadow-xl.card.image-full wrap
+  // insert figure wrap insert img src avatar_url
+  // check on browser to see pic faded.
+  // sibling to figure,   .card-body.justify-end wrap
+  // insert h2.card-title.mb-0 wrap
+  // {name}
+  // check browser 
+  
 
   return (
     <GithubProvider>
