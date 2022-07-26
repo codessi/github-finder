70ea90e266805adfaaefdf24ab294e844e615206
codessi <johansuh@gmail.com>
2022-07-26 16:47:38 -0700

    63 user profile, working

--- src/pages/user.jsx
+++ src/pages/user.jsx
@@ -4,11 +4,11 @@ import { useParams } from "react-router-dom";
 import { FaCodepen, FaStore, FaUserFriends, FaUsers } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import Spinner from "../components/layout/Spinner";
- 
+
 const User = () => {
   const { getUser, user, loading } = useContext(GithubContext);
   const params = useParams();
- 
+
   useEffect(() => {
     getUser(params.login);
   }, []);
@@ -68,6 +68,25 @@ const User = () => {
                 <figure>
                   <img src={avatar_url} alt="avatar" />
                 </figure>
+                <div className="card-body justify-end">
+                  <h2 className="card-title mb-0">{name}</h2>
+                  <p>{login}</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-span-2">
+              <h1 className="text-3xl card-title">
+                {name}
+                <div className="ml-2 mr-1  badge badge-success">
+                  {type}
+                </div>
+                {hireable && (
+                  <div className="mx-1 badge badge-info">Hireable</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className="mt-4 card-actions">
+                <a href={html_url} target="_blank" rel="noreferrer" className="btn btn-primary">Visit Github Profile</a>
               </div>
             </div>
           </div>
@@ -76,6 +95,5 @@ const User = () => {
     );
   }
 };
- 
+
 export default User;
- 
